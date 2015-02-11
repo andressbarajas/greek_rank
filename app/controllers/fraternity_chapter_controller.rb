6a5194ca374b66e1e576a6c3524a7a3f78e317ca
Andy Barajas <andyb@procore.com>
2015-02-10 19:41:01 -0800

    Added default chapter pages

--- app/controllers/fraternity_chapter_controller.rb
+++ app/controllers/fraternity_chapter_controller.rb
@@ -1,7 +1,22 @@
 class FraternityChapterController < ApplicationController
+
+  before_action :set_fraternity_chapter, only: [:show]
+
   def index
   end
 
   def show
+    @fraternity = @chapter.fraternity
   end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_fraternity_chapter
+      @chapter = FraternityChapter.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def fraternity_chapter_params
+      params[:fraternity_chapter]
+    end
 end
--- app/controllers/sorority_chapter_controller.rb
+++ app/controllers/sorority_chapter_controller.rb
@@ -1,7 +1,22 @@
 class SororityChapterController < ApplicationController
+
+  before_action :set_sorority_chapter, only: [:show]
+
   def index
   end
 
   def show
+    @sorority = @chapter.sorority
   end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_sorority_chapter
+      @chapter = SororityChapter.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def sorority_chapter_params
+      params[:sorority_chapter]
+    end
 end
