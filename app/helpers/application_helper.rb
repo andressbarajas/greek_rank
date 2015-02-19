c80fc75da378ecc8a2b1fb68ca2cf97be1ffc20a
Andy Barajas <andyb@procore.com>
2015-02-19 06:18:31 -0800

    Changed background color of uni nav buttons to white. Fixed uni nav button active

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,11 +9,24 @@ module ApplicationHelper
   # end
 
   def nav_pill_link(link_text, link_path)
-    class_name = current_page?(link_path) ? 'active' : ''
+    class_name = check_link_path(link_path) ? 'active' : ''
     class_name += ' category-button'
 
     content_tag(:li, :class => class_name) do
       link_to link_text, link_path
+    end
+  end
+
+private
+  def check_link_path(link_path)
+    if current_page?(link_path)
+      true
+    elsif /\/universities\/\d\/fraternities/.match(link_path) && params[:controller] == "fraternity_chapter"
+      true
+    elsif /\/universities\/\d\/sororities/.match(link_path) && params[:controller] == "sorority_chapter"
+      true
+    else
+      false
     end
   end
 end
